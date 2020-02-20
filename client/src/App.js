26a7f6c1867c808032e242b84720a6d47882d203
dan demeo <chronos937@gmail.com>
2020-02-20 13:26:25 -0500

    added a function to shuffle players then sort player order by that shuffle

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,74 @@
 import React from "react";
 import "./App.css";
+import teams from "./teams.json";
 
-compPlayer = () => {};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  state = {
+    players =[1, 2, 3, 4, 5, 6],
+    playerOrder =[
+      {
+        playerNumber: 1,
+        orderNumber: 0
+      },
+      {
+        playerNumber: 2,
+        orderNumber: 0
+      },
+      {
+        playerNumber: 3,
+        orderNumber: 0
+      },
+      {
+        playerNumber: 4,
+        orderNumber: 0
+      },
+      {
+        playerNumber: 5,
+        orderNumber: 0
+      },
+      {
+        playerNumber: 6,
+        orderNumber: 0
+      }
+    ],
+
+    picker = () => {
+      for (let i = 0; i < players.length; i++) {
+        let j = Math.floor(Math.random() * i + 1);
+        [players[i], players[j]] = [players[j], players[i]];
+      };
+      for (let i = 0; i < playerOrder.length; i++) {
+        playerOrder[i].orderNumber = players[i];
+      }
+      console.log(playerOrder);
+      playerOrder.sort((a, b) => a.orderNumber - b.orderNumber);
+      console.log("----------------------------------------------------");
+      console.log(playerOrder);
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
