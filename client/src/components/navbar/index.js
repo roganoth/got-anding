197cd7b6082492b5f63d1cc7adb88be4943efa7e
Vy Balanzat <vybalanzat@gmail.com>
2020-02-25 21:37:13 -0500

    teamselect

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -6,6 +6,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import DialogSelect from "../TeamSelect/index";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -37,6 +38,7 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             NFL Mock Draft
           </Typography>
+          <DialogSelect color="inherit" />
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
