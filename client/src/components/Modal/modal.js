eb4e40d1f25b879ebd3a47df5801bfed32fa2e34
Vy Balanzat <vybalanzat@gmail.com>
2020-03-04 17:08:17 -0500

    MODAL

---
+++ client/src/components/Modal/modal.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { ListGroup, ListGroupItem } from "reactstrap";
+
+const ModalExample = props => {
+  const { className } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  return (
+    <div>
+      <Button color="info" onClick={toggle}>
+        Team Players
+      </Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Modal TITLE</ModalHeader>
+        <ModalBody>
+          <ListGroup>
+            <ListGroupItem>Cras justo odio</ListGroupItem>
+            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+            <ListGroupItem>Morbi leo risus</ListGroupItem>
+            <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          </ListGroup>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="info" onClick={toggle}>
+            Do Something
+          </Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalExample;
